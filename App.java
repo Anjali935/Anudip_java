17c37841bd8f5a9ec39069de4ada84c7f23dda58
Anjali Tiwari <127283932+Anjali935@users.noreply.github.com>
2023-10-06 15:33:05 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,42 +1,6 @@
-package practise;
+package secondPractiseApp;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-public class App {
-	public static void main(String[] args) {
-		Map<Integer, String> records = new HashMap<>();
-		records.put(101, "Anjali");
-		records.put(102, "sakshi");
-		records.put(103, "Kashish");
-		// System.out.println(records);
-		for (Map.Entry<Integer, String> entry : records.entrySet()) {
-			System.out.println(entry);
-			// System.out.println(entry.getValue());
-			// System.out.println(entry.getKey());
-		}
-		// System.out.println(records.keySet); // It give all the keys
-		// If we insert duplicate keys then last value is assigned to the key value
-		// use null only one time as it update last value with the first value
-		// map to set .It is more efficient to use keySet
-		Set<Integer> keys = new HashSet<>(records.keySet());
-		System.out.println(keys);
-		// map to list
-		List<String> values = new ArrayList<>(records.values());
-		System.out.println(values);
-		//TreeMap will sort bydefault in asscending order 
-		//Hashtable
-		Map<Integer,String> records1=new Hashtable<>();
-		records1.put(106, "Manish");
-		System.out.println(records1);
-		
-		
-		
-		
- 	}
+public class App 
+{
+   
 }
--- Employee.java
+++ Employee.java
@@ -1,17 +1,29 @@
-package practiseArrayList;
-public  class Employee {
+package secondPractiseApp;
+
+import java.util.Objects;
+
+public class Employee {
+	private Long employeeId;
 	private String employeeName;
-    private String employeeGender;
-    private double employeeSalary;
-	public Employee() {
+	private String employeeAddress;
+	private Integer employeeAge;
+	private String employeeGender;
+	private Integer employeeSalary;
+	public Employee(Long employeeId, String employeeName, String employeeAddress, Integer employeeAge,
+			String employeeGender, Integer employeeSalary) {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Employee(String employeeName, String employeeGender, double employeeSalary) {
-		super();
+		this.employeeId = employeeId;
 		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+		this.employeeAge = employeeAge;
 		this.employeeGender = employeeGender;
 		this.employeeSalary = employeeSalary;
+	}
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
 	}
 	public String getEmployeeName() {
 		return employeeName;
@@ -19,25 +31,50 @@ public  class Employee {
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
+	public String getEmployeeAddress() {
+		return employeeAddress;
+	}
+	public void setEmployeeAddress(String employeeAddress) {
+		this.employeeAddress = employeeAddress;
+	}
+	public Integer getEmployeeAge() {
+		return employeeAge;
+	}
+	public void setEmployeeAge(Integer employeeAge) {
+		this.employeeAge = employeeAge;
+	}
 	public String getEmployeeGender() {
 		return employeeGender;
 	}
 	public void setEmployeeGender(String employeeGender) {
 		this.employeeGender = employeeGender;
 	}
-	public double getEmployeeSalary() {
+	public Integer getEmployeeSalary() {
 		return employeeSalary;
 	}
-	public void setEmployeeSalary(double employeeSalary) {
+	public void setEmployeeSalary(Integer employeeSalary) {
 		this.employeeSalary = employeeSalary;
 	}
 	@Override
-	public String toString() {
-		return "Employee [employeeName=" + employeeName + ", employeeGender=" + employeeGender + ", employeeSalary="
-				+ employeeSalary + "]";
+	public int hashCode() {
+		return Objects.hash(employeeId);
 	}
-	
-    
-}
- 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return (employeeId.equals(other.getEmployeeId())&& employeeName.equalsIgnoreCase(null));
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeAddress="
+				+ employeeAddress + ", employeeAge=" + employeeAge + ", employeeGender=" + employeeGender
+				+ ", employeeSalary=" + employeeSalary + "]";
+	}
 
+}
